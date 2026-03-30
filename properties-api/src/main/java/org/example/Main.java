7111d0a34a9359caa8335f921118281e49d0f044
Henrique Eidt <henriqueidt@gmail.com>
2026-03-30 19:22:12 -0300

    get properties from file

--- properties-api/src/main/java/org/example/Main.java
+++ properties-api/src/main/java/org/example/Main.java
@@ -1,5 +1,7 @@
 package org.example;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.Properties;
 
 public class Main {
@@ -12,8 +14,10 @@ public class Main {
         Properties defaults = new Properties();
         defaults.setProperty("animal", "dog");
 
+
         // we can extend default properties
         Properties props1 = new Properties(defaults);
+        props1.setProperty("name", "bruce");
 
 
         // Reading values:
@@ -21,8 +25,22 @@ public class Main {
         System.out.println(animal);
 
 
-        
+        // Iterate over properties
+        for(String key : props1.stringPropertyNames()) {
+            System.out.println(key + " = " + props1.getProperty(key));
+        }
 
-    
+
+        // Reading from a .properties file
+        // Properties files are usually used to store configs like app and host
+        Properties config = new Properties();
+        try (InputStream in = Main.class.getResourceAsStream("/config.properties")) {
+            config.load(in);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println(config.getProperty("app.name"));
+        System.out.println(config.getProperty("db.host"));
     }
 }
