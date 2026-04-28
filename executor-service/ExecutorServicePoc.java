92f8f32deeb8e319306f3a6b3b44c05623b5ccdc
Henrique Eidt <henriqueidt@gmail.com>
2026-04-28 18:51:01 -0300

    execute threads

--- executor-service/ExecutorServicePoc.java
+++ executor-service/ExecutorServicePoc.java
@@ -1,5 +1,22 @@
-public class ExecutorServicePoc {
-    public static void main(String[] args) {
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 
+public class ExecutorServicePoc {
+    public static void main(String[] args) throws InterruptedException {
+
+        // The ExecutorService is a high-level API that manages a `pool of threads`
+        // When submitting tasks, it decides which thread to run them in
+        ExecutorService pool = Executors.newFixedThreadPool(3);
+
+        for (int i = 1; i <= 5; i++) {
+            int taskId = i;
+            pool.execute(() -> {
+                System.out.println("Task " + taskId + " running on " + Thread.currentThread().getName());
+            });
+        }
+
+        pool.shutdown();
+        pool.awaitTermination(5, TimeUnit.SECONDS);
     }
 }
