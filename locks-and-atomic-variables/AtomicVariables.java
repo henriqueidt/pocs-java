3d302f1bd0ae78b83a1485bceb78e0a34839cc10
Henrique Eidt <henriqueidt@gmail.com>
2026-05-18 21:36:40 -0300

    add atomicReference

--- locks-and-atomic-variables/AtomicVariables.java
+++ locks-and-atomic-variables/AtomicVariables.java
@@ -1,5 +1,6 @@
 
 import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.atomic.AtomicReference;
 
 public class AtomicVariables {
   public static void main(String[] args) {
@@ -23,6 +24,29 @@ public class AtomicVariables {
     counter.get(); // reads the current value
     counter.set(10); // sets a value directly
 
-    counter.compareAndSet(5, 10); // Only changes the value to 10, if current value is 5
+    counter.compareAndSet(5, 10); // Only changes the value to 10, if current value is 5 (CAS)
+
+
+    // AtomicReference<T> does the same as AtomicCounter, but changing  an object reference
+    AtomicReference<String> ref = new AtomicReference<>("initial value");
+    ref.get(); // reads actual value
+    ref.set("new value"); // replaces the value
+
+    // A common practice for compareAndSwap is to make an optimistic update
+
+    while (true) { 
+        String actual = ref.get();
+        String newValue = actual + " updated";
+
+        if(ref.compareAndSet(actual, newValue)) {
+          break; // this means it was successfully updated, break the loop
+        }
+        // if other thread changed before, try again
+    }
+
+    // Worth noticing that compareAndSwap uses identity (===), not .equals(),
+    // so the below code will always fail, as they are different objects in memory
+    AtomicReference<String> ref1 = new AtomicReference<>(new String("abc"));
+    ref1.compareAndSet(new String("abc"), "new");
   }
 }
